0b116f3462d7b630812be3b19b8452bb484d78fd
syedshazli <syed.abdu1818@gmail.com>
2026-03-17 14:45:51 -0400

    hello world

---
+++ IntroGo/go.mod
@@ -0,0 +1,5 @@
+module example/IntroGo
+
+go 1.26.1
+
+// Created with "go mod init example/IntroGo"
---
+++ IntroGo/intro.go
@@ -0,0 +1,12 @@
+package main // belongs to executable package
+
+// build with go build
+
+// bring formatting package
+import(
+	"fmt"
+)
+// main function just like C
+func main(){
+	fmt.Println("Hello World! :) ")
+}
