3b70caa4b1b408ed2d54ac02a45d558e95745297
syedshazli <syed.abdu1818@gmail.com>
2026-03-18 12:17:36 -0400

    checkpoint

--- IntroGo/Concurrency/concurrency.go
+++ IntroGo/Concurrency/concurrency.go
@@ -1,17 +1,46 @@
 package concurrency
+import(
+	"fmt"
+	"sync"
+)
+
+
+func printStr( c chan string) {
+	s := <- c 
+	fmt.Println(s);
+	
+}
 
 func main() {
 
-	// TODO task a goroutine to print out a string
+	// task a goroutine to print out a string
+	go fmt.Println("This is a String!")
 
-	// TODO task 5 goroutines to print strings fed into a channel by the main thread
+	// task 5 goroutines to print strings fed into a channel by the main thread
 
-	// TODO using a waitgroup, halt the main thread for a set amount of time using 3 different goroutines
+	myChannel := make(chan string)
+	go printStr( myChannel)
+	go printStr( myChannel)
+	go printStr( myChannel)
+	go printStr( myChannel)
+	go printStr(myChannel)
+	myChannel <- "Hello"
+	myChannel <- "My Name Is"
+	myChannel <- "Syed"
+	myChannel <- "I hope youre doing well"
+	myChannel <- "Bye!"
+
+
+
+
+
+	// using a waitgroup, halt the main thread for a set amount of time using 3 different goroutines
 
 	return
 }
 
 func Buffered_channel(limit int) chan string {
-	// TODO create and return a buffered channel for a passed amount of strings
-	return nil
+	// create and return a buffered channel for a passed amount of strings
+	newChannel := make(chan string, limit)
+	return newChannel
 }
