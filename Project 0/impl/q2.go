f5b3677387bac159c560e5dd1d1a8b470f1d4727
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-03-23 15:41:01 -0400

    done

--- Project 0/impl/q2.go
+++ Project 0/impl/q2.go
@@ -2,10 +2,12 @@ package cs4513_go_impl
 
 import (
 	"bufio"
-	"io"
-	"strconv"
 	"fmt"
-	"strings"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	// "strings"
 )
 
 /*
@@ -15,14 +17,14 @@ Sum numbers from channel `nums` and output sum to `out`.
 You should only output to `out` once.
 */
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
 
-	// sum := 0
-	// for i, := range nums{
-	// 	interSum := <-nums
-	// 	sum += interSum
-	// }
+	sum := 0
+	// This loop runs until the 'nums' channel is closed
+	// Both workers will pull from the same 'nums' pipe simultaneously
+	for n := range nums {
+		sum += n
+	}
+	out <- sum
 }
 
 /*
@@ -38,11 +40,48 @@ func Sum(num int, fileName string) int {
 	// HINT: use `readInts` and `sumWorker`
 	// HINT: use buffered channels for splitting numbers between workers
 
-	// we should probably know how many integers are in the file to find an equal split 
-	readerFromString := strings.NewReader(fileName)
-	result, _ := readInts(readerFromString)
-	fmt.Println(result)
-	return 0
+	// we should probably know how many integers are in the file to find an equal split (found by getting size of resulting array)
+	file, err := os.Open(fileName) // open the file given by the string
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close() // close file when function exits
+
+	result, err := readInts(file)
+
+	if err != nil {
+		fmt.Println("ERROR OCCURED: SEE BELOW")
+		log.Fatal(err)
+	} else {
+		fmt.Println(len(result))
+	}
+
+	// we now know how many results we have to process and how many goroutines need to be launched (telling us how many elements each goroutine needs)
+	numInts := len(result)
+
+	// create output channel for results and buffered input channel to send the results
+	out := make(chan int)
+
+	// Buffer size equal to the number of ints
+	// ensures the sender never blocks.
+	nums := make(chan int, numInts)
+
+	// launch 'num' workers. number of elements they take each is undefined. whoever receives the value first processes and adds to their sum
+	for i := 0; i < num; i++ {
+		go sumWorker(nums, out)
+	}
+
+	for _, v := range result {
+		nums <- v
+	}
+	close(nums)
+
+	collectiveSum := 0
+	for i := 0; i < num; i++ {
+		collectiveSum += <-out // receive output value from each worker and accumulate for each worker
+	}
+
+	return collectiveSum
 }
 
 /*
