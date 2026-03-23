8e10ab00d101a13c4d6fbcc618b5d2b7667d79c6
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-03-23 10:06:06 -0400

    done

--- Project 0/impl/q1.go
+++ Project 0/impl/q1.go
@@ -2,6 +2,9 @@ package cs4513_go_impl
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"regexp"
 	"sort"
 	"strings"
 )
@@ -25,17 +28,84 @@ Parameters:
 You should use `checkError` to handle potential errors.
 */
 func TopWords(path string, numWords int, charThreshold int) []WordCount {
-	// TODO: implement me
+
 	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful.
 	// HINT: the regex "[^0-9a-zA-Z]+" can be used to spot any non-alphanumeric characters.
 
-	path = strings.ToLower(path)
+	if numWords < 1 {
+		return []WordCount{}
+	}
+	// read in file into string
+	// the whole file is now in the 'content' var
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	_ = path
-	_ = numWords
-	_ = charThreshold
+	contentToString := string(content)
 
-	return []WordCount{}
+	// the content of the file is now in lowercase in this string
+	contentLowerCase := strings.ToLower(contentToString)
+	rawTokens := strings.Fields(contentLowerCase) // each element in raawtokens is a whitespaced split word
+
+	var filtered []string
+
+	punctuationRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+
+	for _, token := range rawTokens {
+
+    	cleanWord := punctuationRegex.ReplaceAllString(token, "") // remove punctuation, replace w/nothing
+    
+		// must be greater than or equal to threshold to be counted
+    	if len(cleanWord) >= charThreshold {
+        	filtered = append(filtered, cleanWord)
+    	}
+}
+
+	/*
+		1. remove all non-alphanumeric chars from the string ✅
+		2. Find all words in the string ✅
+		3. See how many times a unique word shows up, counting the word only if len is >= charThreshold ✅
+		4. Make a list of all unique words and their counts, sort by count ✅
+		5. Return/iterate up to k ✅
+	*/
+
+	
+
+	// words is now an array of words that is of len greater than or equal to the threshold
+	words := filtered
+	fmt.Println(words)
+
+	wordMap := make(map[string]int)
+
+	// iterate thru each word in the string. if the word is in the map, increment the value by 1. if not, init with 1
+	for _, word := range words {
+		value, ok := wordMap[word]
+		if ok {
+			wordMap[word] = value + 1
+		} else {
+			wordMap[word] = 1
+		}
+	}
+
+	arrayOfWordCounts := []WordCount{}
+
+	for wordStr, mapCount := range wordMap {
+		newWord := WordCount{Word: wordStr, Count: mapCount}
+		arrayOfWordCounts = append(arrayOfWordCounts, newWord)
+	}
+
+	// this now has an array of structs containing words and their counts, sorted by count in descending order
+	sortWordCounts(arrayOfWordCounts)
+
+	// now, remove any element not in the top 'numWords' elements
+	if len(arrayOfWordCounts) < numWords {
+		numWords = len(arrayOfWordCounts)
+	}
+	arrayOfWordCounts = arrayOfWordCounts[:numWords]
+
+	return arrayOfWordCounts
+
 }
 
 /*
