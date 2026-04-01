d1c7a0c606eab265a7538f2471da807f1bb169a4
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-04-01 13:17:53 -0400

    query passes

--- Project 1/cmd/client/main.go
+++ Project 1/cmd/client/main.go
@@ -6,13 +6,12 @@
 package main
 
 import (
+	"cs4513/project1/types"
 	"flag"
 	"fmt"
 	"log"
 	"net/rpc"
 	"os"
-	"cs4513/project1/coordinator"
-	"cs4513/project1/types"
 )
 
 func main() {
@@ -50,21 +49,23 @@ func cmdSubmit(client *rpc.Client, args []string) {
 		fmt.Fprintln(os.Stderr, "usage: client submit <type> <payload-json>")
 		os.Exit(1)
 	}
-	// TODO: implement
+
 	// Note: The job is processed and sent out  by the coordinator
 	// the JobID is found by querying 'QueryJobs'
-	
+
 	var returnedID types.JobID
 	var jobSpec types.JobSpec
 	jobSpec.Type = args[0]
-	jobSpec.Payload = args[1]
+	jobSpec.Payload = []byte(args[1])
 	err := client.Call("Coordinator.SubmitJob", jobSpec, &returnedID)
 
 	if err != nil {
-		fmt.Println("Error encountered, aborting now")
+		fmt.Fprintf(os.Stderr, "RPC call failed: %v\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Println(returnedID)
+	fmt.Println("Submitted " + returnedID)
+
 }
 
 // cmdQuery prints the current state of one job.
@@ -74,6 +75,29 @@ func cmdQuery(client *rpc.Client, args []string) {
 		os.Exit(1)
 	}
 	// TODO: implement
+
+	// need to call queryJob
+	var currID types.JobID
+	var queryResult types.JobStatus
+
+	currID = types.JobID(args[0])
+	err := client.Call("Coordinator.QueryJob", currID, &queryResult)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Print(queryResult.ID," ", queryResult.State)
+
+	if queryResult.WorkerID != ""{
+		fmt.Print(" ("+queryResult.WorkerID+")")
+	}
+
+	if queryResult.State == 2{ // 2 corresponds to done in 0 index
+		fmt.Println(string(queryResult.Output))
+	} else if queryResult.State == 3{
+		fmt.Println(queryResult.Err)
+	}
+
 }
 
 // cmdList prints all jobs known to the coordinator.
