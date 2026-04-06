631dac6796ecfd9027748bc61610e4d35df62a17
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-04-06 17:32:14 -0400

    jobrecord done!

--- Project 1/coordinator/coordinator.go
+++ Project 1/coordinator/coordinator.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"net/rpc"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -14,31 +15,30 @@ import (
 // jobRecord is the coordinator's private copy of a job and its status.
 type jobRecord struct {
 	jobID types.JobID
-	spec types.JobSpec
+	spec  types.JobSpec
+	state types.JobState
 }
 
 // Coordinator is the RPC server.
 // Keep shared state here and protect it with a mutex.
 type Coordinator struct {
-	mu sync . Mutex
-	jobs map [ types . JobID ]* jobRecord // all jobs , keyed by ID
-	queue [] types . JobID // pending job IDs in FIFO order
-	workers map [ types . WorkerID ] time.Time // registered workers
-	nextJobID int64
+	mu           sync.Mutex
+	jobs         map[types.JobID]*jobRecord   // all jobs , keyed by ID
+	queue        []types.JobID                // pending job IDs in FIFO order
+	workers      map[types.WorkerID]time.Time // registered workers
+	nextJobID    int
 	nextWorkerID int64
-
 }
 
 // New returns an initialized Coordinator.
 // we initialize an empty map of jobs and their records, workers and their IDs, and a queue of job IDs to ex
 func New() *Coordinator {
-	return & Coordinator {
-			jobs : make ( map [ types.JobID ]*jobRecord ) ,
-			workers : make ( map [ types.WorkerID ]time.Time ) ,
-			queue : [] types.JobID {} ,
-}
+	return &Coordinator{
+		jobs:    make(map[types.JobID]*jobRecord),
+		workers: make(map[types.WorkerID]time.Time),
+		queue:   []types.JobID{},
+	}
 
-	
 }
 
 // Start creates a Coordinator, registers it, and starts listening on addr.
@@ -48,16 +48,16 @@ func Start(addr string) error {
 	srv := rpc.NewServer()
 	srv.Register(myCoord) // register the coordinator
 
-	ln, err := net.Listen("tcp", addr )
+	ln, err := net.Listen("tcp", addr)
 
-	if err != nil{
-		return fmt.Errorf("Error listening on address %w: %w", addr, err)
+	if err != nil {
+		return fmt.Errorf("Error listening on address %w: ", err)
 	}
 
-	for {
+	for { // forever loop that accepts connections and sends out go routines
 		conn, err := ln.Accept()
 		// handle err ...
-		if err != nil{
+		if err != nil {
 			return fmt.Errorf("Error connecting to server: %w", err)
 		}
 		go srv.ServeConn(conn)
@@ -68,6 +68,26 @@ func Start(addr string) error {
 // SubmitJob adds a new job and returns its ID.
 func (c *Coordinator) SubmitJob(spec types.JobSpec, reply *types.JobID) error {
 	// TODO: implement
+	c.mu.Lock()
+
+	// new job ID is the nextJobID
+	ID := types.JobID("job-" + strconv.Itoa(c.nextJobID))
+
+	// create a new jobRecord with the new ID and given spec
+	jobRec := jobRecord{ID, spec, types.StatePending}
+
+	// increment the nextJobID
+	c.nextJobID++
+
+	// add the new ID to new jobs
+	c.jobs[ID] = &jobRec
+
+	c.queue = append(c.queue, ID)
+
+	c.mu.Unlock()
+
+	*reply = ID
+
 	return nil
 }
 
