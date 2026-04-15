37ee814f2ae342904f75a6749a75b8e5458e0ff4
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-04-15 12:30:06 -0400

    checkpoint

--- Project2/main/ii/ii.go
+++ Project2/main/ii/ii.go
@@ -1,45 +1,57 @@
-package main
+package mapreduce
 
 import (
-	"fmt"
-	"mr/mapreduce"
+	"hash/fnv"
 	"os"
 )
 
-// mapF is called once per input file. filename is the name of the file being
-// processed and contents is the file's full text. For the inverted index
-// application, mapF should emit one key/value pair per word occurrence,
-// using the word as the key and the filename as the value.
-func mapF(filename string, contents string) (res []mapreduce.KeyValue) {
+// doMap does the job of a map worker: it reads one of the input files
+// (inFile), calls the user-defined map function (mapF) for that file's
+// contents, and partitions the output into nReduce intermediate files.
+func doMap(
+	jobName string, // the name of the MapReduce job
+	mapTaskNumber int, // which map task this is
+	inFile string,
+	nReduce int, // the number of reduce task that will be run ("R" in the paper)
+	mapF func(file string, contents string) []KeyValue,
+) {
 	// TODO:
-	return
+	// You will need to write this function.
+	//
+	// Step 1: Read the input file.
+	//   Use os.ReadFile(inFile) to load the entire file into a byte slice,
+	//   then convert to string: contents := string(data)
+	//   Pass inFile and contents to mapF to get a []KeyValue back.
+	data, _ := os.ReadFile(inFile)
+	contents := string(data)
+	keyVal := mapF(inFile, contents)
+
+	// Step 2: Partition output into nReduce intermediate files.
+	//   For each KeyValue returned by mapF, compute which reduce task it belongs to:
+	//     r := int(ihash(kv.Key)) % nReduce
+	//   The output filename for reduce task r is: reduceName(jobName, mapTaskNumber, r)
+	
+
+	
+	// Step 3: Write each partition using JSON encoding.
+	//   Use json.NewEncoder(file) and enc.Encode(&kv) for each KeyValue.
+	//   Example:
+	//     enc := json.NewEncoder(file)
+	//     for _, kv := range kvs {
+	//       err := enc.Encode(&kv)
+	//       checkError(err)
+	//     }
+	//
+	// Step 4: Write atomically using a temp file + os.Rename.
+	//   See the Note in the project spec (Part A) for the required pattern:
+	//   create each output file with os.CreateTemp, write to it, close it,
+	//   then call os.Rename(tmp.Name(), reduceName(...)) to atomically publish it.
+	//   Use checkError to handle errors.
+
 }
 
-// reduceF is called once per unique word across all input files. key is the
-// word and values is a slice of filenames in which that word appears (may
-// contain duplicates if the word appears multiple times in the same file).
-// reduceF should return a sorted, deduplicated, comma-separated list of
-// document names.
-func reduceF(key string, values []string) string {
-	// TODO:
-	return ""
-}
-
-// Can be run in 2 ways:
-// 1) Sequential (e.g., go run ii/ii.go master sequential x1.txt .. xN.txt)
-// 2) Master   (e.g., go run ii/ii.go master localhost:7777 x1.txt .. xN.txt)
-func main() {
-	if len(os.Args) < 4 {
-		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-	} else if os.Args[1] == "master" {
-		var mr *mapreduce.Master
-		if os.Args[2] == "sequential" {
-			mr = mapreduce.Sequential("iiseq", os.Args[3:], 3, mapF, reduceF)
-		} else {
-			mr = mapreduce.Distributed("iidis", os.Args[3:], 3, os.Args[2])
-		}
-		mr.Wait()
-	} else {
-		mapreduce.RunWorker(os.Args[2], os.Args[3], mapF, reduceF, 100)
-	}
+func ihash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
